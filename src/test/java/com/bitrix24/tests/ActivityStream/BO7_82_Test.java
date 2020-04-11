0d537357251db21f12c7cc583f1a8f4bf894efaf
akbalsedat <akbalsedat@gmail.com>
2020-04-11 10:28:47 -0400

    Create BO7_82_Test.java
    BO7_82_Test.java file is added within tests/ActivityStream package

---
+++ src/test/java/com/bitrix24/tests/ActivityStream/BO7_82_Test.java
@@ -0,0 +1,34 @@
+public class BO7_82_Test extends AbstractBaseTest {
+    @Test
+    public void uploadFileIconClickedTest(){ // AC-1
+        //test --> ExtentTest object
+        //we must add to every test at the beginning
+        //test = report.createTest("Test name");
+        extentTest = extentReports.createTest("Verify upload File Icon is clicked");
+        LoginPage loginPage = new LoginPage();
+        BO7_82_Page bo7_82_page = new BO7_82_Page();
+        loginPage.login();
+        bo7_82_page.clickOnMessage();
+        bo7_82_page.clickOnUploadFile();
+
+        WebElement element = driver.findElement(
+                By.xpath("/html[1]/body[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/form[1]/div[1]/div[1]/div[2]/div[4]"));
+        Assert.assertTrue(element.isDisplayed());
+        //if assertion passed, it will set test status in report to passed
+        extentTest.pass("UploadFile icon clicked was verified");
+    }
+
+    @Test
+    public void addMoreFromContactLists(){ // AC - 2
+        extentTest = extentReports.createTest("Verify upload File Icon is clicked");
+        LoginPage loginPage = new LoginPage();
+        BO7_82_Page bo7_82_page = new BO7_82_Page();
+        loginPage.login();
+        bo7_82_page.clickOnMessage();
+        bo7_82_page.clickOnAddMore();
+        Assert.assertTrue(driver.findElement(
+                By.xpath("//div[@class='bx-finder-box bx-finder-box-vertical bx-lm-box bx-lm-socnet-log-destination']")).isDisplayed());
+        //if assertion passed, it will set test status in report to passed
+        extentTest.pass("UploadFile icon clicked was verified");
+    }
+}
